dd048a510cb898a7bfcccc095ad2c9f237e42ef4
wsr2k2sudo git commit -am dhdhgit config --global user.name wsr2k2 <wsr2k2@gmail.com>
2022-01-20 17:50:41 -0300

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import express from "express";
-import bodyParser from "body-parser";
 import routes from "./routes";
 
 const port = 3034;
@@ -13,4 +12,4 @@ app.use(express.json())
 app.use(routes)
 
 
-app.listen(port, console.log(`Aplicacao rodando em: http://localhost:${port}/users`));
+app.listen(port, console.log(`Aplicacao rodando em: http://localhost:${port}/ceps`));
