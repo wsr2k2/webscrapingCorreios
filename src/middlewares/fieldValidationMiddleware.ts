f51c2e4988dfc9190d6e96109481ab834dd978c7
Luiz Seixas <ticolucas7@gmail.com>
2022-01-24 14:44:57 -0300

    teste

--- src/middlewares/fieldValidationMiddleware.ts
+++ src/middlewares/fieldValidationMiddleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export async function fielValidation(req: Request, res: Response, next: NextFunction) {
 
   if(!req.body.cep) {
-    res.status(400).json({ message: "O cep deve ser preenchido"})
+    res.status(400).json({ message: "O cep deve ser preenchido!"})
     return;
   } if(req.body.cep.length != 8) {
       res.status(400).json({ Error: "O cep deve ser preenchido corretamente com 8 digitos e sem traço" })
